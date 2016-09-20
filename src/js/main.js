2f662136c3d3caf3dda3539a77b3591ef2e11c82
ira <bakum21@ukr.net>
2016-09-20 18:05:58 +0300

    home

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ if ($.browser.safari) $('body').addClass('safari');
 if ($.browser.iphone || $.browser.ipad || $.browser.ipod ) $('body').addClass('ios');
 
 $(document).on('ready', function () {
+
+
 	var $window = $(window),
 		windowWidth = $window.width(),
 		windowHeight = $window.height(),
@@ -19,4 +21,27 @@ $(document).on('ready', function () {
 
 		});
 
+
+
+
+});
+$(window).on('load', function () {
+	//slider home
+	$('.slider_home .owl-carousel').owlCarousel({
+		loop:true,
+		autoplay:true,
+		nav:true,
+		navText: "",
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
 });
