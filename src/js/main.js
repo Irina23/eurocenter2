8fa7e8ed2d9ca6aaf06e840a94464f46b8cdc598
ira <bakum21@ukr.net>
2016-10-28 16:42:37 +0300

    modal city

--- src/js/main.js
+++ src/js/main.js
@@ -36,6 +36,63 @@ $(document).ready( function () {
 		$(this).parent().toggleClass('open');
 	});
 
+	//modal form
+	var overlay = $('#overlay');
+	var open_modal = $('.open_modal');
+	var close = $('.modal_close, #overlay');
+	var modal = $('.modal_div');
+
+	if($(modal).hasClass('show')){
+
+
+			overlay.addClass('show').fadeIn(100,
+				function(){
+					$(modal)
+						.css('display', 'block')
+						.animate({opacity: 1, top: '50%'}, 100);
+					$('body').addClass('no-scroll');
+				});
+
+	}
+
+
+	open_modal.click( function(event){
+		event.preventDefault();
+		var div = $(this).attr('href');
+		overlay.fadeIn(100,
+			function(){
+				$(div)
+					.css('display', 'block')
+					.animate({opacity: 1, top: '50%'}, 100);
+				$('body').addClass('no-scroll');
+			});
+	});
+
+	close.click( function(){
+		modal
+			.animate({opacity: 0, top: '45%'}, 100,
+				function(){
+					$(this).css('display', 'none');
+					overlay.fadeOut(100);
+					$(".message_modal").removeClass("show");
+					$('body').removeClass('no-scroll');
+				}
+			);
+	});
+
+
+	$(this).keydown(function(eventObject){
+		if (eventObject.which == 27)
+			modal.animate({opacity: 0}, 200,
+				function(){
+					$(this).css('display', 'none');
+					overlay.fadeOut(400);
+					$(".message_modal").removeClass('show');
+					$('body').removeClass('no-scroll');
+
+				}
+			);
+	});
 
 });
 
